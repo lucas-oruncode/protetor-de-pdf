427bc61e70f7fda7cdd7f63b531ad900c5b0433f
lucas de jesus <oruncode@gmail.com>
2025-01-16 10:59:18 -0300

    feat: cria modificador de pdf, colocando cpf na página.

---
+++ pdf_modifier.py
@@ -0,0 +1,60 @@
+from PyPDF2 import PdfWriter, PdfReader
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from io import BytesIO
+import os
+
+def modify_pdf(filename, cpf, position, color, upload_folder):
+    packet = BytesIO()
+    can = canvas.Canvas(packet, pagesize=A4)
+
+    if position == 'top-left':
+        x = 50
+        y = 800
+    
+    elif position == 'top-right':
+        x = 500
+        y = 800
+
+    elif position == 'bottom-left':
+        x = 50
+        y = 50
+
+    elif position == 'bottom-right':
+        x = 500
+        y = 50
+    
+    else:
+        raise ValueError('Posição inválida!')
+    
+
+    can.setFillColor(color)
+    can.setFont('Helvetica', 8)
+    can.drawString(x, y, cpf)
+
+    can.save()
+
+
+    try:
+        packet.seek(0)
+        new_pdf = PdfReader(packet)
+        print("Novo PDF criado com sucesso.")
+
+    except Exception as e:
+        print("Erro ao criar um novo PDF.", str(e))
+
+    try:
+        existing_pdf = PdfReader(open(os.path.join(upload_folder, filename), 'rb'))
+        print("PDF aberto.")
+        output = PdfWriter()
+
+        for i in range(len(existing_pdf.pages)):
+            page = existing_pdf.pages[i]
+            page.merge_page(new_pdf.pages[i])
+            output.add_page(page)
+
+        with open(os.path.join(upload_folder, filename), 'wb') as outputStream:
+            output.write(outputStream)    
+        
+    except Exception as e:
+        print("Erro ao abrir PDF existente.", str(e))
